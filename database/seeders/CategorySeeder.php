3ac7c8719737c2a05c2bf2a9c287119cc88fdc9f
Satoshi Kamoshida <duck198771@gmail.com>
2026-09-12 16:30:00 +0900

    Seeder完成

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    public function run(): void
+    {
+        Category::insert([
+            [
+                'content' => '商品のお届けについて',
+            ],
+            [
+                'content' => '商品の交換について',
+            ],
+            [
+                'content' => '商品トラブル',
+            ],
+            [
+                'content' => 'ショップへのお問い合わせ',
+            ],
+            [
+                'content' => 'その他',
+            ],
+        ]);
+    }
+}
---
+++ database/seeders/TagSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Tag;
+use Illuminate\Database\Seeder;
+
+class TagSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Tag::insert([
+            [
+                'name' => '質問',
+            ],
+            [
+                'name' => '要望',
+            ],
+            [
+                'name' => '不具合報告',
+            ],
+            [
+                'name' => 'ご意見',
+            ],
+            [
+                'name' => 'その他',
+            ],
+        ]);
+    }
+}
